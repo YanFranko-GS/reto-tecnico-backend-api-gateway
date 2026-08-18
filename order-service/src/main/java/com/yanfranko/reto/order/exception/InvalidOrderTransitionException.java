bf964c88a2ff9eb0d9029150e478b4e2cefb1b06
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 03:19:30 -0500

    feat: anadimos el manejo de error del cambio de estados

--- order-service/src/main/java/com/yanfranko/reto/order/exception/InvalidOrderTransitionException.java
+++ order-service/src/main/java/com/yanfranko/reto/order/exception/InvalidOrderTransitionException.java
@@ -1,4 +1,19 @@
 package com.yanfranko.reto.order.exception;
 
-public class InvalidOrderTransitionException {
+import com.yanfranko.reto.order.entity.enums.OrderStatus;
+
+public class InvalidOrderTransitionException extends  RuntimeException {
+
+    // este error consiste en que no se puede cambiar de estado que no estan permitidos
+    public InvalidOrderTransitionException (
+        OrderStatus currentStatus,
+        OrderStatus requestedStatus
+    ) {
+        super(
+                "No se permite cambiar de pedido de "
+                + currentStatus
+                + " a "
+                + requestedStatus
+        );
+    }
 }
