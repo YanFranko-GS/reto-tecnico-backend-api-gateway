6991091aae3cd1153a6d7161e04195dfa4d072ac
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-17 19:33:48 -0500

    feat: Añadimos el otro servicio

---
+++ inventory-service/src/test/java/com/yanfranko/reto/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanfranko.reto.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
