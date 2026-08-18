2245d0a2c8aa1b124daf3b6c15e98a8f408f9dd7
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 03:06:52 -0500

    feat: anadimos el manejo de error con el id no encontrado

--- order-service/src/main/java/com/yanfranko/reto/order/exception/OrderNotFoundException.java
+++ order-service/src/main/java/com/yanfranko/reto/order/exception/OrderNotFoundException.java
@@ -1,4 +1,8 @@
 package com.yanfranko.reto.order.exception;
 
-public class OrderNotFoundException {
+public class OrderNotFoundException extends RuntimeException {
+
+    public  OrderNotFoundException(Long orderId) {
+        super ("No se encontro el pedido con el ID: " + orderId);
+    }
 }
