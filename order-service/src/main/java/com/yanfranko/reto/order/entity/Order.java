9bdaeff912e784797023f77a6720c51e57d2dba7
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 00:21:00 -0500

    feat: Creacion de las tablas y contenidos

---
+++ order-service/src/main/java/com/yanfranko/reto/order/entity/Order.java
@@ -0,0 +1,53 @@
+package com.yanfranko.reto.order.entity;
+
+import com.yanfranko.reto.order.entity.enums.OrderStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    // esto lo estoy poniendo para saber la cantidad que se va a pedir
+    @Column(nullable = false)
+    private Integer quantity;
+
+    // eh creado un enum donde contendra todas mis listas de opciones
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatus status;
+
+    // para cuando se creo -- Se vera a mas detalle en el postman
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    // y esto es para la ultima modificacion igual se vera en el postman con mas a detalle
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    // Este es el identificador del producto osea lo que se va a solicitar
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+}
---
+++ order-service/src/main/java/com/yanfranko/reto/order/entity/OrderStatusHistory.java
@@ -0,0 +1,62 @@
+package com.yanfranko.reto.order.entity;
+
+import com.yanfranko.reto.order.entity.enums.OrderStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "order_status_history")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatusHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderstatusId;
+
+    // Esto representa el estado que tendra el estado (antes)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "previous_status", length = 20)
+    private OrderStatus previousStatus;
+
+    // esto cambiara segun el stado anterior (despues)
+    @Enumerated(EnumType.STRING)
+    @Column(name = "new_status", nullable = false, length = 20)
+    private OrderStatus newStatus;
+
+    // esto es para saber cuando se hizo el cambio de estado
+    @Column(nullable = false)
+    private Instant changedAt;
+
+    // esto ya es el motivo del por que se realizo el cambio
+    @Column(length = 255)
+    private String reason;
+
+    // seguimiento de la peticion
+    @Column(name = "trace_id", length = 100)
+    private String traceId;
+
+    // relacion de la tabla order
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+}
