7bc50fc9653963731df3a902df282872aade78e4
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 02:21:20 -0500

    feat: anadiendo dto del order status response

--- order-service/src/main/java/com/yanfranko/reto/order/dto/OrderStatusHistory/OrderHistoryResponseDto.java
+++ order-service/src/main/java/com/yanfranko/reto/order/dto/OrderStatusHistory/OrderHistoryResponseDto.java
@@ -1,4 +1,25 @@
 package com.yanfranko.reto.order.dto.OrderStatusHistory;
 
-public class OrderHistoryResponseDto {
+import com.yanfranko.reto.order.entity.OrderStatusHistory;
+import com.yanfranko.reto.order.entity.enums.OrderStatus;
+
+import java.time.Instant;
+
+public record OrderHistoryResponseDto(
+        Long orderStatusHistoryId,
+        OrderStatus previousStatus,
+        OrderStatus newStatus,
+        Instant changedAt,
+        String reason
+) {
+    // Metodo estático para convertir de Entidad a DTO manteniendo tu estilo
+    public static OrderHistoryResponseDto fromEntity(OrderStatusHistory history) {
+        return new OrderHistoryResponseDto(
+                history.getOrderStatusHistoryId(),
+                history.getPreviousStatus(),
+                history.getNewStatus(),
+                history.getChangedAt(),
+                history.getReason()
+        );
+    }
 }
