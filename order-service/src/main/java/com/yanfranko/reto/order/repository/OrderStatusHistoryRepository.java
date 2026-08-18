0b1509b2fb8c2e3d73afaa14930c550000561130
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 03:03:56 -0500

    feat: anadimos el list para poder listar todas las ordenes con su historial

--- order-service/src/main/java/com/yanfranko/reto/order/repository/OrderStatusHistoryRepository.java
+++ order-service/src/main/java/com/yanfranko/reto/order/repository/OrderStatusHistoryRepository.java
@@ -4,6 +4,10 @@ import com.yanfranko.reto.order.entity.OrderStatusHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderStatusHistoryRepository extends JpaRepository<OrderStatusHistory, Long> {
+
+    List<OrderStatusHistory> findByOrderOrderId(Long orderId);
 }
