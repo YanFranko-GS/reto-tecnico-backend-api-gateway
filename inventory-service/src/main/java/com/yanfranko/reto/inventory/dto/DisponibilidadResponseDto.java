6e1e818f7f309fb8e05a6a7b5003c6e5d2e45c04
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 15:52:39 -0500

    feat: se añadio el inventoryDTO  response de como devolvera la respuesta al ser consultada

--- inventory-service/src/main/java/com/yanfranko/reto/inventory/dto/DisponibilidadResponseDto.java
+++ inventory-service/src/main/java/com/yanfranko/reto/inventory/dto/DisponibilidadResponseDto.java
@@ -3,7 +3,6 @@ package com.yanfranko.reto.inventory.dto;
 
 // dto devolvera los datos del inventario
 public record DisponibilidadResponseDto (
-
         Long productoId,
         Integer cantidadSolicitada,
         Integer stockDisponible,
