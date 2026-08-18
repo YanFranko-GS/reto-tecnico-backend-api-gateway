14b7f67995bddee5c3f4304120fa3377f5739889
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-18 17:14:24 -0500

    feat: se añadio una exception  global para el una mejor descripcion del error usado en ErrorResponse

--- inventory-service/src/main/java/com/yanfranko/reto/inventory/exception/GlobalExceptionHandler.java
+++ inventory-service/src/main/java/com/yanfranko/reto/inventory/exception/GlobalExceptionHandler.java
@@ -1,4 +1,48 @@
 package com.yanfranko.reto.inventory.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductoNoEncontradoException.class)
+    public ResponseEntity<ErrorResponse> handleProductoNoEncontrado(
+            ProductoNoEncontradoException exception
+    ){
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "PRODUCTO_NO_ENCONTRADO",
+                exception.getMessage(),
+                null
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(errorResponse);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleErrorInterno(
+            Exception exception
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                "ERROR_INTERNO",
+                "Se produjo un error interno en el servidor, por favor revisar",
+                null
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(errorResponse);
+    }
+
 }
