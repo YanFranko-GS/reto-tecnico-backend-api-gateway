e7f0f42cc62f0d5ef8851034e0780c297d3262ff
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-20 03:07:47 -0500

    feat: añadimos la trazabilidad en en el order service

--- order-service/src/main/java/com/yanfranko/reto/order/config/TraceIdWebFilter.java
+++ order-service/src/main/java/com/yanfranko/reto/order/config/TraceIdWebFilter.java
@@ -1,4 +1,49 @@
 package com.yanfranko.reto.order.config;
 
-public class TraceIdWebFilter {
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Component
+@Order(-100)
+public class TraceIdWebFilter implements WebFilter {
+
+    private static final String TRACE_ID_HEADER = "X-Trace-Id";
+
+    @Override
+    public Mono<Void> filter(
+            ServerWebExchange exchange,
+            WebFilterChain chain
+    ) {
+
+        String traceId = exchange.getRequest()
+                .getHeaders()
+                .getFirst(TRACE_ID_HEADER);
+
+        if (traceId == null || traceId.isBlank()) {
+            traceId = UUID.randomUUID().toString();
+        }
+
+        ServerHttpRequest request = exchange.getRequest()
+                .mutate()
+                .header(TRACE_ID_HEADER, traceId)
+                .build();
+
+        ServerWebExchange mutatedExchange = exchange
+                .mutate()
+                .request(request)
+                .build();
+
+        mutatedExchange.getResponse()
+                .getHeaders()
+                .set(TRACE_ID_HEADER, traceId);
+
+        return chain.filter(mutatedExchange);
+    }
 }
