a46f63c94162df9b2f14a41d1037f2134a41a09e
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-20 03:04:44 -0500

    feat: configuramos la parte de la trazabilidad en el excepion global

--- inventory-service/src/main/java/com/yanfranko/reto/inventory/exception/GlobalExceptionHandler.java
+++ inventory-service/src/main/java/com/yanfranko/reto/inventory/exception/GlobalExceptionHandler.java
@@ -3,42 +3,64 @@ package com.yanfranko.reto.inventory.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ServerWebExchange;
 
 import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String TRACE_ID_HEADER = "X-Trace-Id";
+
     @ExceptionHandler(ProductoNoEncontradoException.class)
     public ResponseEntity<ErrorResponse> handleProductoNoEncontrado(
-            ProductoNoEncontradoException exception
+            ProductoNoEncontradoException exception,
+            ServerWebExchange exchange
     ){
+        String traceId = exchange.getRequest()
+                .getHeaders()
+                .getFirst(TRACE_ID_HEADER);
+
         ErrorResponse errorResponse = new ErrorResponse(
                 Instant.now(),
                 HttpStatus.NOT_FOUND.value(),
                 "PRODUCTO_NO_ENCONTRADO",
                 exception.getMessage(),
-                null
+                traceId
         );
+
+        exchange.getResponse()
+                .getHeaders().
+                set(TRACE_ID_HEADER, traceId);
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(errorResponse);
     }
 
+
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleErrorInterno(
-            Exception exception
+            Exception exception,
+            ServerWebExchange exchange
     ) {
+        String traceId = exchange.getRequest()
+                .getHeaders()
+                .getFirst(TRACE_ID_HEADER);
+
         ErrorResponse errorResponse = new ErrorResponse(
                 Instant.now(),
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "ERROR_INTERNO",
                 "Se produjo un error interno en el servidor, por favor revisar",
-                null
+                traceId
         );
+
+        exchange.getResponse()
+                .getHeaders()
+                .set(TRACE_ID_HEADER, traceId);
 
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
