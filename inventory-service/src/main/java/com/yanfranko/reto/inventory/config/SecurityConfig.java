36a101b07b01105a7d8626d69bf7a1323ac203fd
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-20 15:56:10 -0500

    feat: anadimos la ruta y protegemos con el oauth2

--- inventory-service/src/main/java/com/yanfranko/reto/inventory/config/SecurityConfig.java
+++ inventory-service/src/main/java/com/yanfranko/reto/inventory/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.yanfranko.reto.inventory.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -17,8 +18,14 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
-                        .anyExchange().permitAll()
+                        .pathMatchers("/actuator/health").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(Customizer.withDefaults())
                 )
                 .build();
     }
+
+    // aqui tambien aplicammos la misma config como en los demas modulos
 }
