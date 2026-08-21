e206432cf08d5540a02786693243489f30a01289
YanFranko-GS <gonzalessegurayanfranko@gmail.com>
2026-08-21 08:51:25 -0500

    feat: añadimos los monitoreos de cada ejecucion del service

--- inventory-service/src/main/java/com/yanfranko/reto/inventory/aspect/ServiceLoggingAspect.java
+++ inventory-service/src/main/java/com/yanfranko/reto/inventory/aspect/ServiceLoggingAspect.java
@@ -1,4 +1,55 @@
 package com.yanfranko.reto.inventory.aspect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
 public class ServiceLoggingAspect {
+
+
+    // se crea un funcion mas que todo para registrar los logs de los metodos de service
+    //desde Iniciando y finalizando para medir el tiempo de demoro en milisegundo
+    @Around("execution(* com.yanfranko.reto.inventory.service..*(..))")
+    public Object logServiceExecution(ProceedingJoinPoint joinPoint)
+            throws  Throwable {
+
+        String methodName = joinPoint.getSignature().toShortString();
+
+        long start = System.currentTimeMillis();
+
+        log.info("Iniciando {}", methodName);
+
+        try {
+            Object result = joinPoint.proceed();
+
+            long duration = System.currentTimeMillis() - start;
+
+            log.info(
+                    "Finalizando {} - duración={} ms"
+                    , methodName,
+                    duration
+            );
+            return  result;
+        }
+
+        catch (Exception exception) {
+            long duration = System.currentTimeMillis() - start;
+
+            log.error(
+                    "Error en {} - duración={} ms - mensaje={}",
+                    methodName,
+                    duration,
+                    exception.getMessage()
+            );
+
+            throw exception;
+        }
+
+
+    }
 }
